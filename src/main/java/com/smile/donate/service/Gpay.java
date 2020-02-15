79ba843012fcfd4e9704da6143f0382660a3e2b4
vinod-nagaraj <57495482+vinod-nagaraj@users.noreply.github.com>
2020-02-15 12:48:12 +0530

    Update Gpay.java

--- src/main/java/com/smile/donate/service/Gpay.java
+++ src/main/java/com/smile/donate/service/Gpay.java
@@ -1,7 +1,7 @@
 package com.smile.donate.service;
 
 import com.smile.donate.constant.ApplicationConstants;
-
+@Service("GPay")
 public class Gpay implements PaymentService {
 	@Override
 	public String payment(String paymentType) {
